1ae2d874faad3c7f32e0820ae360b3c3bdc8ca07
MannRobertJ <mann.robertj@gmail.com>
2019-01-26 09:27:33 +0100

    years now appear in select menu

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
-import React, { Component } from 'react';
-import './App.css';
-
-const data = {
-  "Ivel Z3": {
-    manufacturer: "Ivasim",
-    year: 1969,
-    origin: "Croatia"
-  },
-  "Bally Astrocade": {
-    manufacturer: "Bally Consumer Products",
-    year: 1977,
-    origin: "USA"
-  },
-  "Sord M200 Smart Home Computer": {
-    manufacturer: "Sord Computer Corporation",
-    year: 1971,
-    origin: "Japan"
-  },
-  "Commodore 64": {
-    manufacturer: "Commodore",
-    year: 1982,
-    origin: "USA"
-  }
-}
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  render() {
+  state = {
+    data: {
+      "Ivel Z3": {
+        manufacturer: "Ivasim",
+        year: 1969,
+        origin: "Croatia"
+      },
+      "Bally Astrocade": {
+        manufacturer: "Bally Consumer Products",
+        year: 1977,
+        origin: "USA"
+      },
+      "Sord M200 Smart Home Computer": {
+        manufacturer: "Sord Computer Corporation",
+        year: 1971,
+        origin: "Japan"
+      },
+      "Commodore 64": {
+        manufacturer: "Commodore",
+        year: 1982,
+        origin: "USA"
+      }
+    }
+  };
+
+  render = () => {
     return (
       <div className="App">
         <select>
-          <option value = ""> -- pick a model </option>
-          {
-            Object.keys(data).map(name => <option value={name}>{name}</option>)
-          }
+          <option value=""> -- pick a model </option>
+          {Object.keys(this.state.data).map(name => (
+            <option value={name}>{`${name} (${this.state.data[name].year})`}</option>
+          ))}
         </select>
       </div>
     );
